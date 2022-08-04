868de670cf653dfa6e7c761108bd9715d5aa0139
dori-dev <mr.dori.dev@gmail.com>
2022-08-04 15:24:15 +0430

    add scraper to django news application

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^aa7o%!5_rlaf&0kx(*c)a*!@29_jx$n7a7%*64w9de-clor#q'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'sample_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -122,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+API_KEY = os.environ.get('API_KEY', 'sample_api_key')
--- news/scraper.py
+++ news/scraper.py
@@ -1,3 +1,16 @@
+import json
+import requests
+from requests import exceptions
+from django.conf import settings
+
+API = "https://api.rss2json.com/v1/api.json"
+api_data = {
+    'rss_url': '',
+    'api_key': settings.API_KEY,
+    'count': 50,
+}
+TIMEOUT = 2
+# should get from admin site and database(with default)
 rss_urls = {
     "home": {
         "isna": "https://www.isna.ir/rss-homepage",
@@ -24,3 +37,47 @@ rss_urls = {
         "selected_news": "https://www.tasnimnews.com/fa/rss/feed/0/9/0/%D8%A7%D8%AE%D8%A8%D8%A7%D8%B1-%D8%A8%D8%B1%DA%AF%D8%B2%DB%8C%D8%AF%D9%87"
     }
 }
+
+
+def extract_data(item: dict):
+    title = item["title"]
+    pubDate = item["pubDate"]
+    link = item["link"]
+    guid = item["guid"]
+    author = item["author"]
+    thumbnail = item["thumbnail"]
+    description = item["description"]
+    content = item["content"]
+    enclosure: dict = item["enclosure"]
+    categories: list = item["categories"]
+    return (
+        title, pubDate, link, guid,
+        author, thumbnail, description,
+        content, enclosure, categories
+    )
+
+
+count = 0
+for rss_url in rss_urls:
+    api_data['rss_url'] = rss_url
+    try:
+        response = requests.post(
+            API,
+            data=api_data,
+            timeout=TIMEOUT
+        )
+    except (exceptions.ConnectionError, exceptions.ReadTimeout):
+        continue
+    data = json.loads(response.text)
+    items = data['items']
+    for item in items:
+        count += 1
+        try:
+            the_item = extract_data(item)
+        except KeyError:
+            continue
+        else:
+            pass
+            # print(the_item)
+
+print(count)
